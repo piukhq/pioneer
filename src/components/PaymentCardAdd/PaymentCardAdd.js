548c7f958a03865e468a42b2166ac3d72b756aa3
Lehel Kacso <lkacso@bink.com>
2020-12-21 17:23:49 +0000

    Create component to add payment card

---
+++ src/components/PaymentCardAdd/PaymentCardAdd.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import styles from './PaymentCardAdd.module.scss'
+
+const PaymentCardAdd = () => (
+  <div className={styles['payment-card-add']}>
+    <div className={styles['payment-card-add__border']}>
+      <div className={styles['payment-card-add__inner']}>
+      </div>
+    </div>
+  </div>
+)
+
+export default PaymentCardAdd
