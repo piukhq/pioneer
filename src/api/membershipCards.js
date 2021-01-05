1a912e3b9acd641d41ec087ee5d7578425e05535
Lehel Kacso <lkacso@bink.com>
2021-01-05 14:47:47 +0000

    Create temporary dev component to recreate removed membership card to test user

--- src/api/membershipCards.js
+++ src/api/membershipCards.js
@@ -10,3 +10,31 @@ export const getMembershipCards = () => (
     },
   )
 )
+
+// todo: to remove. This functionality was added because every now and then someone or something would remove the
+// membership card for the test user account
+export const addTestMembershipCard = () => (
+  axios.post(
+    'https://api.dev.gb.bink.com/ubiquity/membership_cards',
+    {
+      account: {
+        add_fields: [
+          {
+            column: 'Membership card number',
+            value: '1048172852',
+          }],
+        authorise_fields: [
+          {
+            column: 'Email',
+            value: 'binktestuser14@wasabi.com',
+          }],
+      },
+      membership_plan: '315',
+    },
+    {
+      headers: {
+        Authorization: `Token ${localStorage.getItem('token')}`,
+      },
+    },
+  )
+)
--- src/components/MembershipCards/MembershipCards.js
+++ src/components/MembershipCards/MembershipCards.js
@@ -1,7 +1,25 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+import { addTestMembershipCard } from 'api/membershipCards'
 import { actions as membershipCardsActions, selectors as membershipCardsSelectors } from 'ducks/membershipCards'
+
+// todo: this is a temporary way of quickly recreate a membership card. To be removed once the functionality to addind a membership card was intorduced
+const RecreateRemovedMembershipCard = () => {
+  const dispatch = useDispatch()
+  const membershipCards = useSelector(state => membershipCardsSelectors.cardsList(state))
+  return (!membershipCards || membershipCards.length === 0) && (
+    <>
+      DEV mode only. No membership card was found.
+      <button onClick={async () => {
+        await addTestMembershipCard()
+        dispatch(membershipCardsActions.getMembershipCards())
+      } }>
+        Create one
+      </button>
+    </>
+  )
+}
 
 const MembershipCards = ({ onError }) => {
   const dispatch = useDispatch()
@@ -24,6 +42,7 @@ const MembershipCards = ({ onError }) => {
           </Link>
         </div>
       ))}
+      <RecreateRemovedMembershipCard />
     </>
   )
 }
