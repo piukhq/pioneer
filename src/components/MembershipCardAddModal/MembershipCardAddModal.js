db1e3d1e02d90f743cdae364ef6b2216b10c4c8e
Lehel Kacso <lkacso@bink.com>
2021-02-01 10:17:44 +0000

    Adjust the title and summary and add the loading state for the Add and Enrol membership card modals

--- src/components/MembershipCardAddModal/MembershipCardAddModal.js
+++ src/components/MembershipCardAddModal/MembershipCardAddModal.js
@@ -4,16 +4,19 @@ import useLoadMembershipPlans from './hooks/useLoadMembershipPlans'
 import useCloseModalOnSuccess from './hooks/useCloseModalOnSuccess'
 import Loading from 'components/Loading'
 import MembershipCardForm from 'components/MembershipCardForm'
+import { useMembershipCardsState } from 'hooks/membershipCards'
 
 const MembershipCardAddModal = ({ onClose, planId }) => {
   const { plan, loading } = useLoadMembershipPlans(planId)
+  const { add: { loading: addLoading } } = useMembershipCardsState()
   useCloseModalOnSuccess(onClose)
   const fieldTypes = useRef(['add_fields', 'authorise_fields']).current
 
   return (
     <Modal onClose={onClose}>
-      { loading && <Loading /> }
+      { (loading || addLoading) && <Loading /> }
       <Modal.Header>Add your card</Modal.Header>
+      <p>Please enter the following information to add your existing card to your account.</p>
       <MembershipCardForm
         plan={plan}
         planId={planId}
--- src/components/MembershipCardEnrolModal/MembershipCardEnrolModal.js
+++ src/components/MembershipCardEnrolModal/MembershipCardEnrolModal.js
@@ -4,17 +4,19 @@ import useLoadMembershipPlans from './hooks/useLoadMembershipPlans'
 import useCloseModalOnSuccess from './hooks/useCloseModalOnSuccess'
 import Loading from 'components/Loading'
 import MembershipCardForm from 'components/MembershipCardForm'
+import { useMembershipCardsState } from 'hooks/membershipCards'
 
 const MembershipCardEnrolModal = ({ onClose, planId }) => {
   const { plan, loading } = useLoadMembershipPlans(planId)
+  const { add: { loading: addLoading } } = useMembershipCardsState()
   useCloseModalOnSuccess(onClose)
   const fieldTypes = useRef(['enrol_fields']).current
 
   return (
     <Modal onClose={onClose}>
-      { loading && <Loading /> }
-      {/* todo: to decide the title of the modal */}
-      <Modal.Header>Sign up for new card</Modal.Header>
+      { (loading || addLoading) && <Loading /> }
+      <Modal.Header>Sign up for { plan?.account?.plan_name }</Modal.Header>
+      <p>{ plan?.account?.plan_summary }</p>
       <MembershipCardForm
         plan={plan}
         planId={planId}
