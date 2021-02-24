0885018bf50e337445efc0255ef5bb44de167e37
Lehel Kacso <lkacso@bink.com>
2021-02-24 18:05:30 +0000

    Use client_id and bundle_id per theme

--- binkConfig/default.js
+++ binkConfig/default.js
@@ -14,4 +14,16 @@ module.exports = {
       case 'wasabi': return true
     }
   })(),
+  clientId: (() => {
+    switch (process.env.THEME) {
+      case 'bink': return 'MKd3FfDGBi1CIUQwtahmPap64lneCa2R6GvVWKg6dNg4w9Jnpd'
+      case 'wasabi': return 'KY6ia4AvWwl9GXnKfPMqHJy7U3vUE2pSpDjJaqazZ0LZCHu5dj'
+    }
+  })(),
+  bundleId: (() => {
+    switch (process.env.THEME) {
+      case 'bink': return 'com.bink.wallet'
+      case 'wasabi': return 'com.wasabi.bink.web'
+    }
+  })(),
 }
