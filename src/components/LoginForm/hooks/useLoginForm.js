da8d587152ad4e7344334f8d7d6541da38a4c54c
Lehel Kacso <lkacso@bink.com>
2021-02-24 12:18:53 +0000

    Remove pre-populated credentials from builds. It will still be there for development environment for now

--- src/components/LoginForm/hooks/useLoginForm.js
+++ src/components/LoginForm/hooks/useLoginForm.js
@@ -3,8 +3,8 @@ import { useUsersDispatch } from 'hooks/users'
 
 const useLoginForm = () => {
   // todo: to remove default values for username and password
-  const [email, setEmail] = useState('bink_web_user_1@bink.com')
-  const [password, setPassword] = useState('BinkWeb01')
+  const [email, setEmail] = useState(process.env.NODE_ENV === 'development' ? 'bink_web_user_2@bink.com' : '')
+  const [password, setPassword] = useState(process.env.NODE_ENV === 'development' ? 'BinkWeb01' : '')
 
   const { login } = useUsersDispatch()
   const submit = () => login(email, password)
