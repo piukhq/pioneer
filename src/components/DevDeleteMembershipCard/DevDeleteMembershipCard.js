d5d5ac3ffcd8457b6e05827bd901ca64c5596f0f
Lehel Kacso <lkacso@bink.com>
2021-02-25 12:28:43 +0000

    Add specific look and feel to dev-only elements

--- src/components/DevDeleteMembershipCard/DevDeleteMembershipCard.js
+++ src/components/DevDeleteMembershipCard/DevDeleteMembershipCard.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useMembershipCardsDispatch } from 'hooks/membershipCards'
+import cx from 'classnames'
 import styles from './DevDeleteMembershipCard.module.scss'
 
 const DevDeleteMembershipCard = ({ cardId }) => {
   const { deleteMembershipCard } = useMembershipCardsDispatch()
   return (
     process.env.NODE_ENV === 'development' ? (
-      <button className={styles.root} onClick={() => deleteMembershipCard(cardId)}>Delete card with id {cardId}</button>
+      <button className={cx(styles.root, 'dev-only')} onClick={() => deleteMembershipCard(cardId)}>Delete card with id {cardId}</button>
     ) : null
   )
 }
--- src/components/MembershipCards/MembershipCards.js
+++ src/components/MembershipCards/MembershipCards.js
@@ -46,7 +46,9 @@ const MembershipCards = ({ onError }) => {
 
           {/* todo: temporary to allow adding cards more easily in dev mode */}
           { process.env.NODE_ENV === 'development' && (
-            <Link to={`/membership-card/add/${Config.membershipPlanId}`}>Add a card</Link>
+            <div className='dev-only'>
+              <Link to={`/membership-card/add/${Config.membershipPlanId}`}>Add a card</Link>
+            </div>
           ) }
         </>
       ) }
