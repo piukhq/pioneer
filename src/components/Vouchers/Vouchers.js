a7b1ef9e2c7795d9135bea91dd492eb1fbab6b09
Lehel Kacso <lkacso@bink.com>
2021-02-26 17:47:19 +0000

    Modal for voucher history

--- src/components/Vouchers/Vouchers.js
+++ src/components/Vouchers/Vouchers.js
@@ -1,31 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useMembershipCardStateById } from 'hooks/membershipCards'
 import useLoadMembershipPlans from './hooks/useLoadMembershipPlans'
+import Button from 'components/Button'
+import NonActiveVouchersModal from 'components/NonActiveVouchersModal'
 
 const Vouchers = ({ membershipCardId }) => {
   const { activeVouchers, nonActiveVouchers, plan } = useMembershipCardStateById(membershipCardId)
   useLoadMembershipPlans()
+  const [isNonActiveVouchersModalOpen, setNonActiveVouchersModalOpen] = useState(false)
 
   if (!plan?.has_vouchers || !activeVouchers || activeVouchers.length === 0) {
     return null
   }
   return (
-    <div>
-      <h2>Active vouchers</h2>
-      { activeVouchers.map?.((voucher, index) => (
-        <div key={index}>
-          <div>state: {voucher.state} ({voucher?.earn?.value}/{voucher?.earn?.target_value} {voucher?.earn?.suffix})</div>
-        </div>
-      )) }
-      {/* TODO: temporary. to remove once history is implemented */}
-      { process.env.NODE_ENV === 'development' && (
-        nonActiveVouchers?.length > 0 && (
-          <div style={{ color: '#999', fontSize: '0.8rem' }} className='dev-only'>
-            {nonActiveVouchers.length} non active cards in history
+    plan?.has_vouchers && (activeVouchers.length > 0 || nonActiveVouchers.length > 0) ? (
+      <div>
+        <h2>Vouchers</h2>
+        { activeVouchers.map?.((voucher, index) => (
+          <div key={index}>
+            <div>state: {voucher.state} ({voucher?.earn?.value}/{voucher?.earn?.target_value} {voucher?.earn?.suffix})</div>
           </div>
-        )
-      )}
-    </div>
+        )) }
+        { nonActiveVouchers?.length > 0 && (
+          <Button onClick={() => setNonActiveVouchersModalOpen(true)}>View voucher history</Button>
+        ) }
+        { isNonActiveVouchersModalOpen && (
+          <NonActiveVouchersModal
+            membershipCardId={membershipCardId}
+            onClose={() => setNonActiveVouchersModalOpen(false)}
+          />
+        )}
+      </div>
+    ) : null
   )
 }
 
