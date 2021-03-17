897ef5f991382652738bc8f5fcbe080ff3c45362
Lehel Kacso <lkacso@bink.com>
2021-03-17 17:06:32 +0000

    Create custom hook for RequestMagicLink component

--- src/components/RequestMagicLink/RequestMagicLink.js
+++ src/components/RequestMagicLink/RequestMagicLink.js
@@ -1,23 +1,23 @@
-import React, { useState, useCallback } from 'react'
+import React from 'react'
 import TextInputGroup from 'components/Form/TextInputGroup'
-import { actions as usersActions } from 'ducks/users'
-import { useDispatch, useSelector } from 'react-redux'
 
 import { ReactComponent as MagicLinkDefaultSvg } from 'images/magic-link-default.svg'
 import { ReactComponent as MagicLinkErrorSvg } from 'images/magic-link-error.svg'
 import Button from 'components/Button'
 import Loading from 'components/Loading'
+import useRequestMagicLink from './hooks/useRequestMagicLink'
 
 import styles from './RequestMagicLink.module.scss'
 
 const RequestMagicLink = () => {
-  const [email, setEmail] = useState('')
-  const { error, loading, success } = useSelector(state => state.users.magicLinkRequest)
-  const dispatch = useDispatch()
-  const handleSubmit = useCallback((event) => {
-    event.preventDefault()
-    dispatch(usersActions.requestMagicLink(email))
-  }, [dispatch, email])
+  const {
+    email,
+    setEmail,
+    success,
+    loading,
+    error,
+    handleSubmit,
+  } = useRequestMagicLink()
 
   return (
     <>
@@ -69,7 +69,8 @@ const RequestMagicLink = () => {
               Click the link and you’ll be signed in.
             </p>
             <p className={styles.root__paragraph}>
-              <span className={styles.root__note}>Note:</span> The device you open the link on will be the device you are signed in on.
+              <span className={styles.root__note}>Note:</span>{' '}
+              The device you open the link on will be the device you are signed in on.
               For example, if you open the link on your phone you will be logged in on your phone.
             </p>
           </div>
