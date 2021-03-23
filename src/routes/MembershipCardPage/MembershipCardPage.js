9bba69b888398dca04693262a20d0646063553e3
Lukie Kang <lkang@bink.com>
2021-03-23 13:57:11 +0000

    fix trailling space

--- src/routes/MembershipCardPage/MembershipCardPage.js
+++ src/routes/MembershipCardPage/MembershipCardPage.js
@@ -144,7 +144,7 @@ const MembershipCardPage = () => {
                   onClick={handleClickOnPaymentCard}
                   key={paymentCard.id}
                   expired={isPaymentCardExpired(paymentCard)}
-                  activating={(paymentCard.status === 'pending' && !isPaymentCardExpired(paymentCard))}  
+                  activating={(paymentCard.status === 'pending' && !isPaymentCardExpired(paymentCard))}
                 />
               ))
             }
