79affdb6d626bad36e4c566437e385e30784c0c9
Lehel Kacso <lkacso@bink.com>
2021-03-23 17:36:27 +0000

    Refactor the redirect logic  on the Magic Link page

--- src/routes/MagicLinkPage/MagicLinkPage.js
+++ src/routes/MagicLinkPage/MagicLinkPage.js
@@ -1,47 +1,9 @@
-import React, { useEffect } from 'react'
-import { useHistory, useParams } from 'react-router-dom'
-import { isTokenUsed } from 'utils/magicLink'
-import { actions as usersActions } from 'ducks/users'
+import React from 'react'
 import Loading from 'components/Loading'
-import { useDispatch, useSelector } from 'react-redux'
-
-const useRedirectOnUsedToken = () => {
-  const { magicLinkToken } = useParams()
-
-  const history = useHistory()
-  useEffect(() => {
-    if (isTokenUsed(magicLinkToken)) {
-      history.replace('/')
-    }
-  }, [magicLinkToken, history])
-}
+import useRedirectLogic from './hooks/useRedirectLogic'
 
 const MagicLinkPage = () => {
-  useRedirectOnUsedToken()
-
-  const { loading, error, success } = useSelector(state => state.users.magicLinkAuthentication)
-
-  const dispatch = useDispatch()
-  const { magicLinkToken } = useParams()
-
-  useEffect(() => {
-    if (!isTokenUsed(magicLinkToken)) {
-      dispatch(usersActions.magicLinkAuthentication(magicLinkToken))
-    }
-  }, [magicLinkToken, dispatch])
-
-  const history = useHistory()
-  useEffect(() => {
-    if (error) {
-      history.replace('/login')
-    }
-  }, [error, history])
-
-  useEffect(() => {
-    if (success) {
-      history.replace('/')
-    }
-  }, [success, history])
+  const { loading } = useRedirectLogic()
 
   return (
     loading ? <Loading /> : null
