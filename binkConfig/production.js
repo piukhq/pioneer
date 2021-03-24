cfa4880fe47a7561e32cb540c041c489af66b3d4
Lehel Kacso <lkacso@bink.com>
2021-03-24 09:22:34 +0000

    Updated the wasabi plan id for the wasabi white-label build in staging and production

--- binkConfig/production.js
+++ binkConfig/production.js
@@ -1,4 +1,11 @@
 module.exports = {
   apiUrl: 'https://api.gb.bink.com',
   env: 'production',
+  membershipPlanId: (() => {
+    // note: if overwritten then all cases must be kept
+    switch (process.env.THEME) {
+      case 'bink': return null
+      case 'wasabi': return 215
+    }
+  })(),
 }
--- binkConfig/staging.js
+++ binkConfig/staging.js
@@ -1,6 +1,13 @@
 module.exports = {
   apiUrl: 'https://api.staging.gb.bink.com',
   env: 'staging',
+  membershipPlanId: (() => {
+    // note: if overwritten then all cases must be kept
+    switch (process.env.THEME) {
+      case 'bink': return null
+      case 'wasabi': return 281
+    }
+  })(),
   clientId: (() => {
     switch (process.env.THEME) {
       case 'bink': return 'MKd3FfDGBi1CIUQwtahmPap64lneCa2R6GvVWKg6dNg4w9Jnpd'
