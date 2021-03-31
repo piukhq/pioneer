c5eb93ee237c9f16b83ec80b1ab0061c0b8f5424
Lehel Kacso <lkacso@bink.com>
2021-03-31 13:24:16 +0100

    Update primary button styling. Button styling is per theme now.

---
+++ src/routes/TypographyPage/ButtonExample/ButtonExample.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from 'components/Button'
+
+const ButtonExample = () => (
+  <div>
+    <Button primary>Primary button</Button>
+    <Button primary disabled>Disabled button</Button>
+    <Button secondary>Secondary button</Button>
+    <Button tertiary>Tertiary button</Button>
+  </div>
+)
+
+export default ButtonExample
