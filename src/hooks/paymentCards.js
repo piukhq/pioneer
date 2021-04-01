a4ab9af088fcd0de20fb6223ed5d857f36e8012c
Lukie Kang <lkang@bink.com>
2021-04-01 06:47:22 +0100

    add getPaymentCardsDispatch helper method

--- src/hooks/paymentCards.js
+++ src/hooks/paymentCards.js
@@ -1,4 +1,8 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+
+import {
+  actions as paymentCardsActions,
+} from 'ducks/paymentCards'
 
 export const usePaymentCardsState = () => ({
   cards: useSelector(state => state.paymentCards.cards),
@@ -8,5 +12,12 @@ export const usePaymentCardById = (cardId) => {
   const { cards } = usePaymentCardsState()
   return {
     card: cards[cardId],
+  }
+}
+
+export const useGetPaymentCardsDispatch = () => {
+  const dispatch = useDispatch()
+  return {
+    getPaymentCards: () => dispatch(paymentCardsActions.getPaymentCards()),
   }
 }
