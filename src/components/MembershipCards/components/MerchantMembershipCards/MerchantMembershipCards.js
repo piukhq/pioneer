9aabbb26b23c4774d567adbcd332f629eb6ffd62
Lukie Kang <lkang@bink.com>
2021-04-07 15:26:38 +0100

    moved useContactSupport into a local hook

--- src/components/MembershipCards/components/MerchantMembershipCards/MerchantMembershipCards.js
+++ src/components/MembershipCards/components/MerchantMembershipCards/MerchantMembershipCards.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from 'components/Button'
 import useLogout from 'hooks/useLogout'
-import useContactSupport from 'hooks/useContactSupport'
+import useContactSupport from './hooks/useContactSupport'
 import cx from 'classnames'
 
 import styles from './MerchantMembershipCards.module.scss'
@@ -16,6 +16,7 @@ const MerchantMembershipCards = () => {
       <h1 className={cx(styles.root__heading, styles['root__heading--first'])}>There is a problem</h1>
       <p className={styles.root__body}>It looks like there is a problem with your account.</p>
       <p className={styles.root__body}>Please contact us so we can help resolve this as quickly as possible.</p>
+      {/* todo: consider replacing button with link tag to match its functionality */}
       <Button onClick={contactSupport} className={styles.root__wide}>Get in Touch</Button>
       <button className={styles.root__logout} onClick={logout}>Logout</button>
       { process.env.NODE_ENV === 'development' && (
