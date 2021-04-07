e8ebf448710f1fb18b83197e4696765d1f006e2a
Lukie Kang <lkang@bink.com>
2021-04-07 14:59:48 +0100

    fix indentation, remove development dev list component

--- src/components/MembershipCards/components/MultichannelMembershipCards/MultichannelMembershipCards.js
+++ src/components/MembershipCards/components/MultichannelMembershipCards/MultichannelMembershipCards.js
@@ -13,32 +13,26 @@ const MultichannelMembershipCards = () => {
 
   return (
     <div className={styles.root}>
-       <h1 className={cx(styles.root__heading, styles['root__heading--first'])}>Membership Cards</h1>
+      <h1 className={cx(styles.root__heading, styles['root__heading--first'])}>Membership Cards</h1>
       {membershipCards.map(card => (
-      <div key={card.id}>
-        <Link to={`/membership-card/${card.id}`}>
-          Id: {card.card.membership_id}
-        </Link>{' '}
-        <span style={{ color: '#999', fontSize: '0.7rem', textDecoration: 'none' }}>
-          (plan: {card.membership_plan}){' '}
-          (id: {card.id})
-          ({card?.status?.state})
-        </span>
-      </div>
+        <div key={card.id}>
+          <Link to={`/membership-card/${card.id}`}>
+            Id: {card.card.membership_id}
+          </Link>{' '}
+          <span style={{ color: '#999', fontSize: '0.7rem', textDecoration: 'none' }}>
+            (plan: {card.membership_plan}){' '}
+            (id: {card.id})
+            ({card?.status?.state})
+          </span>
+        </div>
       ))}
-       <Link to={`/membership-card/add/${Config.membershipPlanId}`}>Add a card</Link>
-        <br /><br /><br />
-        <Link to={'/payment-cards'}>Payment Cards</Link>
-        <br />
-        <Link to={'/membership-plans'}>Membership Plans</Link>
-    <button onClick={logout}>Logout</button>
-    { membershipCards.length > 1 && process.env.NODE_ENV === 'development' && (
-    <div className="dev-only">
-      <DevMembershipCardsList/>
+      <Link to={`/membership-card/add/${Config.membershipPlanId}`}>Add a card</Link>
+      <br /><br /><br />
+      <Link to={'/payment-cards'}>Payment Cards</Link>
+      <br />
+      <Link to={'/membership-plans'}>Membership Plans</Link>
+      <button onClick={logout}>Logout</button>
     </div>
-    )}
-    {/* <RecreateRemovedMembershipCard /> */}
-  </div>
   )
 }
 
