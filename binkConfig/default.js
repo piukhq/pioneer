2d129dee7b4e73d787eccacefa79b806a6d41ea2
Lukie Kang <lkang@bink.com>
2021-04-08 12:56:33 +0100

    add planTitle and MagicLinkRequestFormDescription config elements

--- binkConfig/default.js
+++ binkConfig/default.js
@@ -40,6 +40,18 @@ module.exports = {
       case 'wasabi': return 'https://www.wasabi.uk.com/contact-us/'
     }
   })(),
+  planTitle: (() => {
+    switch (process.env.THEME) {
+      case 'bink': return 'Access your loyalty account'
+      case 'wasabi': return 'Wasabi Club'
+    }
+  })(),
+  magicLinkRequestFormDescription: (() => {
+    switch (process.env.THEME) {
+      case 'bink': return ['Get a link sent to your inbox so you can register or login instantly!']
+      case 'wasabi': return ['Collect 7 stamps for a £7 OFF Meal Voucher. One stamp can be earned per customer per transaction when the qualifying spend in a single transaction for food & drink purchases totals £7.00 or more in a participating Wasabi store. Ts and Cs apply.', 'Bink is a service which links payment cards to loyalty memberships allowing you to earn rewards automatically when you shop. By entering your email address below you authorise Bink to check if you already have a loyalty account.', 'Log in or become a new member just by entering your email address']
+    }
+  })(),
   // dev only (and likely temporary)
   devDefaultUser: (() => {
     switch (process.env.THEME) {
