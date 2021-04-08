6b388b2eb4f701eee37dbcd5d362c799eb62cdec
Lukie Kang <lkang@bink.com>
2021-04-08 07:44:01 +0100

    move membershipcards component code to membershipcards page, delete rendundant membershipcards component

--- src/components/MerchantMembershipCards/MerchantMembershipCards.js
+++ src/components/MerchantMembershipCards/MerchantMembershipCards.js
@@ -3,6 +3,8 @@ import Button from 'components/Button'
 import useLogout from 'hooks/useLogout'
 import useContactSupport from './hooks/useContactSupport'
 import cx from 'classnames'
+import Config from 'Config'
+import { useMembershipCardsState } from 'hooks/membershipCards'
 
 import styles from './MerchantMembershipCards.module.scss'
 import MultichannelMembershipCards from '../MultichannelMembershipCards'
@@ -10,14 +12,17 @@ import MultichannelMembershipCards from '../MultichannelMembershipCards'
 const MerchantMembershipCards = () => {
   const { contactSupport } = useContactSupport()
   const { logout } = useLogout()
-
-  return (
+  const { membershipCards } = useMembershipCardsState()
+  const cards = membershipCards.filter(
+    card => card.membership_plan === Config.membershipPlanId,
+  )
+  return !cards ? null : (
     <div className={styles.root}>
       <h1 className={cx(styles.root__heading, styles['root__heading--first'])}>There is a problem</h1>
       <p className={styles.root__body}>It looks like there is a problem with your account.</p>
       <p className={styles.root__body}>Please contact us so we can help resolve this as quickly as possible.</p>
       {/* todo: consider replacing button with link tag to match its functionality */}
-      <Button onClick={contactSupport} className={styles.root__wide}>Get in Touch</Button>
+      <Button onClick={contactSupport} className={cx(styles['root__button--wide'])}>Get in Touch</Button>
       <button className={styles.root__logout} onClick={logout}>Logout</button>
       {/* todo: remove development only section */}
       { process.env.NODE_ENV === 'development' && (
--- src/routes/MembershipCardsPage/MembershipCardsPage.js
+++ src/routes/MembershipCardsPage/MembershipCardsPage.js
@@ -1,19 +1,34 @@
 import React, { useCallback, useState } from 'react'
-import MembershipCards from 'components/MembershipCards'
+import { useMembershipCardsState } from 'hooks/membershipCards'
+import useLoadMembershipCards from './hooks/useLoadMembershipCards'
+
+import Config from 'Config'
+import Loading from 'components/Loading'
+
+import MerchantMembershipCards from 'components/MerchantMembershipCards'
+import MultichannelMembershipCards from 'components/MultichannelMembershipCards'
 
 const MembershipCardsPage = () => {
   const [error, setError] = useState(false)
   const handleError = useCallback(() => {
     setError(true)
   }, [setError])
-  return (
-    <div>
-      { error ? (
-        <p>There was an error</p>
-      ) : (
-        <MembershipCards onError={handleError}/>
-      ) }
-    </div>
+
+  const { loading } = useMembershipCardsState()
+  useLoadMembershipCards(handleError)
+  return error ? <p>There was an error</p> : (
+    <>
+    { !Config.isMerchantChannel && (
+      <MultichannelMembershipCards />
+    ) }
+    { Config.isMerchantChannel && (
+      <MerchantMembershipCards />
+    ) }
+     {/* todo: to decide on the visuals of loading */}
+    { loading && (
+      <Loading />
+    ) }
+  </>
   )
 }
 
