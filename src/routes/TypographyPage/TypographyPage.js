e7e8c06895f319102a2904afc7b81fff16161de0
Lehel Kacso <lkacso@bink.com>
2021-04-13 11:16:30 +0100

    Add the new loading animation to the Typography page

--- src/routes/TypographyPage/TypographyPage.js
+++ src/routes/TypographyPage/TypographyPage.js
@@ -4,11 +4,13 @@ import BodyExample from './BodyExample'
 import ButtonExample from './ButtonExample'
 import ModalExample from './ModalExample'
 import TextInputGroupExample from 'routes/TypographyPage/TextInputGroupExample'
+import Loading2 from 'components/Loading2'
 
 const TypographyPage = () => (
   <>
     <Heading />
     <BodyExample />
+    <Loading2 />
     <ButtonExample />
     <TextInputGroupExample />
     <ModalExample />
