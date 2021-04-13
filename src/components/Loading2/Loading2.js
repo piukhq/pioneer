ef72ce7b370da266175dad5aeb5981e20749a1e9
Lehel Kacso <lkacso@bink.com>
2021-04-13 11:16:00 +0100

    Implement the new Loading animation

---
+++ src/components/Loading2/Loading2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { ReactComponent as LoaderSvg } from 'images/loader.svg'
+import styles from './Loading2.module.scss'
+
+const Loading2 = () => (
+  <LoaderSvg className={styles.root} />
+)
+
+export default Loading2
