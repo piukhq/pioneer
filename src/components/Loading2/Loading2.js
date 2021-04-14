9de68bb30386cbcee7a93ba07800f30c7865139a
Lehel Kacso <lkacso@bink.com>
2021-04-14 09:36:04 +0100

    Set a max-width to the description text on HangTight. Add a todo regarding Loading2 component name

--- src/components/Loading2/Loading2.js
+++ src/components/Loading2/Loading2.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { ReactComponent as LoaderSvg } from 'images/loader.svg'
 import styles from './Loading2.module.scss'
 
+// todo:
+//  - This could gradually replace the <Loading/> component. Depends on future designs.
+//  - Once the replacement is finished, this component could be renamed to Loading
+
 const Loading2 = () => (
   <LoaderSvg className={styles.root} />
 )
