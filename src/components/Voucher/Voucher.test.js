1cf6335fa37f57dd94e30b0d7db5d5c87db55fd9
George O'Donnell <george@c02f5a0emd6m.local>
2021-04-22 16:29:05 +0100

    Adding additional tests

--- src/components/Voucher/Voucher.test.js
+++ src/components/Voucher/Voucher.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ShallowRenderer from 'react-test-renderer/shallow'
 import Voucher from './Voucher'
+import styles from './Voucher.module.scss'
 
 describe("Test 'Voucher' component", () => {
   const defaultProps = {
@@ -16,12 +17,43 @@ describe("Test 'Voucher' component", () => {
 
   let shallowRender
   beforeEach(() => {
+    jest.resetModules()
     shallowRender = createShallowRenderer()
   })
 
-  describe('Test output', () => {
-    it('should render a div', () => {
+  describe('Test rendered output', () => {
+    it('should render parent div container', () => {
       expect(shallowRender.type).toBe('div')
+      expect(shallowRender.props.className).toBe(styles.root)
+    })
+
+    it('should render children div containers', () => {
+      const [title, description, headline, progress] = shallowRender.props.children
+
+      expect(title.type).toBe('div')
+      expect(title.props.className).toBe(styles.root__title)
+
+      expect(description.type).toBe('div')
+      expect(description.props.className).toBe(styles.root__description)
+
+      expect(headline.type).toBe('div')
+      expect(headline.props.className).toBe(styles.root__headline)
+
+      expect(progress.type).toBe('div')
+      expect(progress.props.className).toBe(styles.root__progress)
+    })
+
+    it('should render the correct number of progress identifiers', () => {
+      const mockTargetValue = 5
+      const mockVoucher = {
+        earn: {
+          target_value: mockTargetValue,
+        },
+      }
+      shallowRender = createShallowRenderer({ voucher: mockVoucher })
+
+      const [, , , progress] = shallowRender.props.children
+      expect(progress.props.children.length).toEqual(mockTargetValue)
     })
   })
 })
