7d3d5d5990f3ebc6a925a3f95d8d6a0de2e817d2
Lukie Kang <lkang@bink.com>
2021-04-27 15:08:53 +0100

    minor changes following code review.

--- src/components/PreparingYourCard/PreparingYourCard.js
+++ src/components/PreparingYourCard/PreparingYourCard.js
@@ -8,13 +8,13 @@ const PreparingYourCard = () => (
   <div className={styles.root}>
     <h1 className={styles.root__heading}>Preparing your card</h1>
     <div className={styles.root__description}>
-      <p>We are just getting your card set up.</p>
-      <p>This won't take long and we will redirect you automatically when this is done.</p>
+      <p className={styles['root__description-paragraph']}>We are just getting your card set up.</p>
+      <p className={styles['root__description-paragraph']}>This won't take long and we will redirect you automatically when this is done.</p>
     </div>
     <div className={styles.root__loading}>
       <Loading2/>
     </div>
-    <p className={styles.root__support}>If this is taking longer than you expect, <a href={Config.urls.support}>reach out to us</a> and we can help.</p>
+    <p className={styles.root__support}>If this is taking longer than you expect, <a className={styles['root__support-link']} href={Config.urls.support} target="_blank" rel="noreferrer">reach out to us</a> and we can help.</p>
   </div>
 )
 
