b858ae13519f90c218929b6d32e28ff3fdaa2414
Lukie Kang <lkang@bink.com>
2021-05-11 12:04:59 +0100

    refactor conditional logic and use enrol check hook

--- src/components/MerchantMembershipCardEnrol/MerchantMembershipCardEnrol.js
+++ src/components/MerchantMembershipCardEnrol/MerchantMembershipCardEnrol.js
@@ -2,7 +2,7 @@ import React, { useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { selectors as usersSelectors } from 'ducks/users'
 import useLoadMembershipPlans from './hooks/useLoadMembershipPlans'
-import useLoadMembershipCardsStatus from './hooks/useLoadMembershipCardsStatus'
+import useLoadMembershipCardsReenrol from './hooks/useLoadMembershipCardsReenrol'
 import MembershipCardForm from 'components/MembershipCardForm'
 
 import useContactSupport from 'hooks/useContactSupport'
@@ -13,7 +13,7 @@ import styles from './MerchantMembershipCardEnrol.module.scss'
 
 const MerchantMembershipCardEnrol = ({ planId }) => {
   const { plan } = useLoadMembershipPlans(planId)
-  const { isReenrol } = useLoadMembershipCardsStatus()
+  const { isReenrol } = useLoadMembershipCardsReenrol()
   const { logout } = useLogout()
   const fieldTypes = useRef(['enrol_fields']).current
   const linkingFeature = 'ENROL'
@@ -24,21 +24,12 @@ const MerchantMembershipCardEnrol = ({ planId }) => {
   const initialValues = useRef({ enrol_fields: { Email: userId } }).current
   const disabledFields = useRef({ enrol_fields: { Email: true } }).current
 
+  const [headerText, summaryText] = isReenrol ? ['Let\'s try again', 'There was a problem getting your card set up. Please try again. Remember, we are always here to help if you would rather us help resolve this.'] : [plan?.account?.plan_name, plan?.account?.plan_summary]
+
   return (
     <div className={styles.root}>
-      {isReenrol && (
-        <>
-          <h1 className={styles.root__header}>Let's try again</h1>
-          <p className={styles.root__summary}>There was a problem getting your card set up. Please try again. Remember, we are always here to help if you would rather us help resolve this.</p>
-        </>
-      )}
-      {!isReenrol && (
-        <>
-          <h1 className={styles.root__header}>Join {plan?.account.plan_name}</h1>
-          <p className={styles.root__summary}>{ plan?.account?.plan_summary }</p>
-        </>
-      )}
-
+      <h1 className={styles.root__header}>{headerText}</h1>
+      <p className={styles.root__summary}>{summaryText}</p>
       <MembershipCardForm
         plan={plan}
         planId={planId}
@@ -49,10 +40,9 @@ const MerchantMembershipCardEnrol = ({ planId }) => {
         submitCaption='Register'
         submittingCaption='Registering'
       />
-
       {isReenrol && <Button secondary onClick={contactSupport} className={styles['root__alternate-option']}>Contact Support</Button>}
       {!isReenrol && <Button secondary onClick={logout} className={styles['root__alternate-option']}>Cancel</Button>}
-    </div>
+  </div>
   )
 }
 
