05affeaf9ee631c348718a4728efd1165e02890f
Lukie Kang <lkang@bink.com>
2021-05-19 15:23:40 +0100

    refactor to remove redundant code

--- src/routes/TypographyPage/NoteExample/NoteExample.js
+++ src/routes/TypographyPage/NoteExample/NoteExample.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import cx from 'classnames'
 import styles from './NoteExample.module.scss'
 
 const NoteExample = () => (
   <div>
-    <h1 className={cx(styles.root__note, styles['root__note--first'])}>This is note text. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi et eveniet laudantium, molestiae nisi similique! Aut distinctio dolores impedit incidunt.</h1>
+    <p className={styles.root__note}>This is note text. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi et eveniet laudantium, molestiae nisi similique! Aut distinctio dolores impedit incidunt.</p>
   </div>
 )
 
