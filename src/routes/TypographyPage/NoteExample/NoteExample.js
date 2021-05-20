4077e122e5c7d7359572538711b1d351d40238df
Lukie Kang <lkang@bink.com>
2021-05-20 14:32:57 +0100

    remove unneeded div. add verical padding to help distinguish note eleemnt frm others.

--- src/routes/TypographyPage/NoteExample/NoteExample.js
+++ src/routes/TypographyPage/NoteExample/NoteExample.js
@@ -2,9 +2,7 @@ import React from 'react'
 import styles from './NoteExample.module.scss'
 
 const NoteExample = () => (
-  <div>
     <p className={styles.root__note}>This is note text. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi et eveniet laudantium, molestiae nisi similique! Aut distinctio dolores impedit incidunt.</p>
-  </div>
 )
 
 export default NoteExample
