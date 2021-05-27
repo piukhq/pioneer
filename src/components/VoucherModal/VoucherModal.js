5810b806cff055a523dff7498f1d8a9cf52c585a
Lukie Kang <lkang@bink.com>
2021-05-27 15:28:43 +0100

    add issuedDetail from API. Add placeholder to prevent jarring modal movement on image load

--- src/components/VoucherModal/VoucherModal.js
+++ src/components/VoucherModal/VoucherModal.js
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Modal from 'components/Modal'
 
 import styles from './VoucherModal.module.scss'
 
 const VoucherModal = ({ onClose, voucher, plan }) => {
+  const [imageLoading, setImageLoading] = useState(true)
   const issueDate = new Date(voucher.date_issued)
   const expiryDate = new Date(voucher.expiry_date)
 
   const formattedIssueDate = `${issueDate.getDate()} ${issueDate.toLocaleString('en-uk', { month: 'short' })} ${issueDate.getFullYear()}`
   const formattedExpiryDate = `${expiryDate.getDate()} ${expiryDate.toLocaleString('en-uk', { month: 'short' })} ${expiryDate.getFullYear()}`
   const imgUrl = plan?.images?.filter(image => image.type === 9)[0].url
+  const issuedDetail = plan.content.filter(content => content.column === 'Voucher_Issued_Detail')[0].value
 
   return (
   <Modal className={styles.root} onClose={onClose}>
-    { imgUrl && <img className={styles.root__image} src={imgUrl} alt='' /> }
-    <p className={styles.root__heading}>Free Meal for collecting 7 stamps.</p>
+    {imageLoading && <div className={styles.root__placeholder} />}
+    <img className={styles.root__image} src={imgUrl} alt='' onLoad={ () => setImageLoading(false)}/>
+    <p className={styles.root__heading}>{voucher?.burn?.prefix}{voucher?.burn?.value} {voucher?.burn?.suffix} for collecting {voucher?.earn?.prefix}{voucher?.earn?.target_value} {voucher?.earn?.suffix}</p>
     <p className={styles.root__code}>{voucher.code}</p>
     <div className={styles.root__details}>
-      <p className={styles.root__paragraph}>Use the code shown to redeem your reward.</p>
+      <p className={styles.root__paragraph}>{issuedDetail}</p>
       <p className={styles.root__paragraph}>Added {formattedIssueDate}</p>
       <p className={styles.root__paragraph}>Expires {formattedExpiryDate}</p>
     </div>
