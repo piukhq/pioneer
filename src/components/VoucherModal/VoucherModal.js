a61c0ee1d029a126d6f8c1caea43af97060c9e50
Lukie Kang <lkang@bink.com>
2021-05-28 08:00:15 +0100

    refactor to use destructured values from voucher in render

--- src/components/VoucherModal/VoucherModal.js
+++ src/components/VoucherModal/VoucherModal.js
@@ -11,12 +11,14 @@ const VoucherModal = ({ onClose, voucher, plan }) => {
   const formattedExpiryDate = dayShortMonthYear(new Date(voucher.expiry_date))
   const imgUrl = plan?.images?.filter(image => image.type === 9)[0].url
   const issuedDetail = plan.content.filter(content => content.column === 'Voucher_Issued_Detail')[0].value
+  const { prefix: burnPrefix, value: burnValue, suffix: burnSuffix } = voucher.burn
+  const { prefix: earnPrefix, target_value: earnTargetValue, suffix: earnSuffix } = voucher.earn
 
   return (
   <Modal className={styles.root} onClose={onClose}>
     {imageLoading && <div className={styles.root__placeholder} />}
     <img className={styles.root__image} src={imgUrl} alt='' onLoad={ () => setImageLoading(false)}/>
-    <p className={styles.root__heading}>{voucher?.burn?.prefix}{voucher?.burn?.value} {voucher?.burn?.suffix} for collecting {voucher?.earn?.prefix}{voucher?.earn?.target_value} {voucher?.earn?.suffix}</p>
+    <p className={styles.root__heading}>{burnPrefix}{burnValue} {burnSuffix} for collecting {earnPrefix}{earnTargetValue} {earnSuffix}</p>
     <p className={styles.root__code}>{voucher.code}</p>
     <div className={styles.root__information}>
       <p className={styles['root__information__issued-detail']}>{issuedDetail}</p>
