7fc5cc496866a4a1a9cbc751749d51e5cd2c56f9
Lukie Kang <lkang@bink.com>
2021-05-28 09:24:34 +0100

    refactor of voucher modal to use destructuring more

--- src/components/VoucherModal/VoucherModal.js
+++ src/components/VoucherModal/VoucherModal.js
@@ -7,19 +7,20 @@ import styles from './VoucherModal.module.scss'
 const VoucherModal = ({ onClose, voucher, plan }) => {
   const [imageLoading, setImageLoading] = useState(true)
 
-  const formattedIssueDate = dayShortMonthYear(new Date(voucher.date_issued))
-  const formattedExpiryDate = dayShortMonthYear(new Date(voucher.expiry_date))
+  const formattedIssueDate = dayShortMonthYear(new Date(voucher?.date_issued))
+  const formattedExpiryDate = dayShortMonthYear(new Date(voucher?.expiry_date))
   const imgUrl = plan?.images?.filter(image => image.type === 9)[0].url
-  const issuedDetail = plan.content.filter(content => content.column === 'Voucher_Issued_Detail')[0].value
-  const { prefix: burnPrefix, value: burnValue, suffix: burnSuffix } = voucher.burn
-  const { prefix: earnPrefix, target_value: earnTargetValue, suffix: earnSuffix } = voucher.earn
+  const issuedDetail = plan?.content?.filter(content => content.column === 'Voucher_Issued_Detail')[0].value
+  const { code } = voucher?.code
+  const { prefix: burnPrefix, value: burnValue, suffix: burnSuffix } = voucher?.burn
+  const { prefix: earnPrefix, target_value: earnTargetValue, suffix: earnSuffix } = voucher?.earn
 
   return (
   <Modal className={styles.root} onClose={onClose}>
     {imageLoading && <div className={styles.root__placeholder} />}
     <img className={styles.root__image} src={imgUrl} alt='' onLoad={ () => setImageLoading(false)}/>
     <p className={styles.root__heading}>{burnPrefix}{burnValue} {burnSuffix} for collecting {earnPrefix}{earnTargetValue} {earnSuffix}</p>
-    <p className={styles.root__code}>{voucher.code}</p>
+    <p className={styles.root__code}>{code}</p>
     <div className={styles.root__information}>
       <p className={styles['root__information__issued-detail']}>{issuedDetail}</p>
       <p className={styles.root__information__date}>Added {formattedIssueDate}</p>
