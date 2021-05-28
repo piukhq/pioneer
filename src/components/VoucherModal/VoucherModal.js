a00c755f06eb09092295b21fad7bb5e23aed276a
Lukie Kang <lkang@bink.com>
2021-05-28 11:56:54 +0100

    revised destructuring to account for missing voucher values

--- src/components/VoucherModal/VoucherModal.js
+++ src/components/VoucherModal/VoucherModal.js
@@ -7,27 +7,28 @@ import styles from './VoucherModal.module.scss'
 const VoucherModal = ({ onClose, voucher, plan }) => {
   const [imageLoading, setImageLoading] = useState(true)
 
-  const formattedIssueDate = dayShortMonthYear(new Date(voucher?.date_issued))
-  const formattedExpiryDate = dayShortMonthYear(new Date(voucher?.expiry_date))
+  const { code = '', burn = {}, earn = {}, date_issued = null, expiry_date = null } = voucher
+  const { prefix: burnPrefix, value: burnValue, suffix: burnSuffix } = burn
+  const { prefix: earnPrefix, target_value: earnTargetValue, suffix: earnSuffix } = earn
+  const formattedIssueDate = dayShortMonthYear(new Date(date_issued))
+  const formattedExpiryDate = dayShortMonthYear(new Date(expiry_date))
+
   const imgUrl = plan?.images?.filter(image => image.type === 9)[0].url
   const issuedDetail = plan?.content?.filter(content => content.column === 'Voucher_Issued_Detail')[0].value
-  const { code } = voucher?.code
-  const { prefix: burnPrefix, value: burnValue, suffix: burnSuffix } = voucher?.burn
-  const { prefix: earnPrefix, target_value: earnTargetValue, suffix: earnSuffix } = voucher?.earn
 
   return (
-  <Modal className={styles.root} onClose={onClose}>
-    {imageLoading && <div className={styles.root__placeholder} />}
-    <img className={styles.root__image} src={imgUrl} alt='' onLoad={ () => setImageLoading(false)}/>
-    <p className={styles.root__heading}>{burnPrefix}{burnValue} {burnSuffix} for collecting {earnPrefix}{earnTargetValue} {earnSuffix}</p>
-    <p className={styles.root__code}>{code}</p>
-    <div className={styles.root__information}>
-      <p className={styles['root__information__issued-detail']}>{issuedDetail}</p>
-      <p className={styles.root__information__date}>Added {formattedIssueDate}</p>
-      <p className={styles.root__information__date}>Expires {formattedExpiryDate}</p>
-    </div>
-    <a className={styles.root__link} href={Config.urls.termsAndConditions}>Terms & Conditions</a>
-  </Modal>
+    <Modal className={styles.root} onClose={onClose}>
+      {imageLoading && <div className={styles.root__placeholder} />}
+      <img className={styles.root__image} src={imgUrl} alt='' onLoad={ () => setImageLoading(false)}/>
+      <p className={styles.root__heading}>{burnPrefix}{burnValue} {burnSuffix} for collecting {earnPrefix}{earnTargetValue} {earnSuffix}</p>
+      <p className={styles.root__code}>{code}</p>
+      <div className={styles.root__information}>
+        <p className={styles['root__information__issued-detail']}>{issuedDetail}</p>
+        <p className={styles.root__information__date}>Added {formattedIssueDate}</p>
+        <p className={styles.root__information__date}>Expires {formattedExpiryDate}</p>
+      </div>
+      <a className={styles.root__link} href={Config.urls.termsAndConditions}>Terms & Conditions</a>
+    </Modal>
   )
 }
 
