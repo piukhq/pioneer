8c0a85f7b270eafa0e06a116766a236125cdab4b
George <godonnell@bink.com>
2021-05-31 11:30:06 +0100

    WIP displaying only redeemable vouchers

--- src/components/Vouchers/Vouchers.js
+++ src/components/Vouchers/Vouchers.js
@@ -5,25 +5,27 @@ import Voucher from 'components/Voucher'
 
 import styles from './Vouchers.module.scss'
 
-const Vouchers = ({ membershipCardId }) => {
+const Vouchers = ({ membershipCardId, displayRedeemableOnly = false }) => {
   const { activeVouchers, plan } = useMembershipCardStateById(membershipCardId)
   useLoadMembershipPlans()
 
-  if (!plan?.has_vouchers || !activeVouchers || activeVouchers.length === 0) {
-    return null
+  const renderVouchers = () => {
+    // State is probably different
+    const vouchersToDisplay = displayRedeemableOnly ? activeVouchers.filter(voucher => voucher.state === 'earned') : activeVouchers
+
+    return vouchersToDisplay.map?.((voucher, index) => (
+      <Voucher key={index} voucher={voucher} />
+    ))
   }
+
   return (
-    plan?.has_vouchers && (activeVouchers.length > 0) ? (
-      <div>
-        <h2 className={styles.root__headline}>Vouchers</h2>
-        <p className={styles.root__paragraph}>{plan.account?.plan_summary}</p>
-        <div className={styles['root__active-vouchers']}>
-        { activeVouchers.map?.((voucher, index) => (
-          <Voucher key={index} voucher={voucher} />
-        )) }
-        </div>
+    <>
+      <h2 className={styles.root__headline}>Vouchers</h2>
+      <p className={styles.root__paragraph}>{plan.account?.plan_summary}</p>
+      <div className={styles['root__active-vouchers']}>
+        {renderVouchers()}
       </div>
-    ) : null
+    </>
   )
 }
 
