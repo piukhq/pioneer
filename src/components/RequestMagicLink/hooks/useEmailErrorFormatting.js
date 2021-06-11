404b69e46ad917d853da491ba47dba24b8d62a37
Lukie Kang <lkang@bink.com>
2021-06-11 07:29:03 +0100

    split logic for email field into own hook, revert useRequestMagicLink to original version

---
+++ src/components/RequestMagicLink/hooks/useEmailErrorFormatting.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { isValidEmail } from 'utils/validation'
+
+const useEmailErrorFormatting = (email, setEmail) => {
+  const [emailError, setEmailError] = useState(undefined)
+
+  const handleBlur = () => {
+    setEmailError(isValidEmail(email) ? undefined : ' ')
+  }
+
+  const handleChange = (event) => {
+    setEmailError(undefined)
+    setEmail(event.target.value)
+  }
+
+  return {
+    emailError,
+    handleChange,
+    handleBlur,
+  }
+}
+
+export default useEmailErrorFormatting
--- src/components/RequestMagicLink/hooks/useRequestMagicLink.js
+++ src/components/RequestMagicLink/hooks/useRequestMagicLink.js
@@ -1,37 +1,22 @@
 import { useState, useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { actions as usersActions } from 'ducks/users'
-import { isValidEmail } from 'utils/validation'
 
 const useRequestMagicLink = () => {
   const [email, setEmail] = useState('')
-  const [emailError, setEmailError] = useState(undefined)
   const { error, loading, success } = useSelector(state => state.users.magicLinkRequest)
   const dispatch = useDispatch()
-
-  const handleBlur = () => {
-    setEmailError(isValidEmail(email) ? undefined : ' ')
-  }
-
-  const handleChange = (event) => {
-    setEmailError(undefined)
-    setEmail(event.target.value)
-  }
   const handleSubmit = useCallback((event) => {
     event.preventDefault()
     dispatch(usersActions.requestMagicLink(email))
   }, [dispatch, email])
 
   return {
-    isValidEmail,
     email,
     setEmail,
     error,
     loading,
     success,
-    emailError,
-    handleChange,
-    handleBlur,
     handleSubmit,
   }
 }
