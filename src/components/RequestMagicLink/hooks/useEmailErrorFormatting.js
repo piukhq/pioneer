5ec0062d7532e217fae775c5acb38b404389d024
Lukie Kang <lkang@bink.com>
2021-06-11 11:13:33 +0100

    add use of booleans to simplify logic

--- src/components/RequestMagicLink/hooks/useEmailErrorFormatting.js
+++ src/components/RequestMagicLink/hooks/useEmailErrorFormatting.js
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import { isValidEmail } from 'utils/validation'
 
 const useEmailErrorFormatting = (email, setEmail) => {
-  const [emailError, setEmailError] = useState(undefined)
+  const [emailError, setEmailError] = useState(false)
 
   const handleBlur = () => {
-    setEmailError(isValidEmail(email) ? undefined : ' ')
+    setEmailError(!isValidEmail(email))
   }
 
   const handleChange = (event) => {
-    emailError !== undefined && setEmailError(undefined)
+    emailError && setEmailError(false)
     setEmail(event.target.value)
   }
 
