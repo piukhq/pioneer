69a2918ae117600af7af9129dc0511541a51fd44
Lukie Kang <lkang@bink.com>
2021-07-21 13:22:10 +0100

    fix linting issues

--- src/components/PaymentCard/PaymentCard.test.js
+++ src/components/PaymentCard/PaymentCard.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { fireEvent, render, screen} from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { usePaymentCardById } from 'hooks/paymentCards'
 import PaymentCard from './PaymentCard'
 
