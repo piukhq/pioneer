e2ffccbdbe58ab386eee1bd81f831ac6a111614e
George <godonnell@bink.com>
2021-07-27 14:27:17 +0100

    Replacing p element tags with div tags as overriding the wasabi p styles does not seem possible

--- src/components/MembershipCardContainer/components/RewardsHistory/RewardsHistory.js
+++ src/components/MembershipCardContainer/components/RewardsHistory/RewardsHistory.js
@@ -30,8 +30,8 @@ const RewardsHistory = ({ membershipCard, state, addPaymentCardClickHandler = ()
               {/* todo: would there ever be an unhappy path ever where balance is missing? */}
               <div data-testid='transaction-history' className={styles['root__transaction-history']} onClick={() => setTransactionsModalOpen(true)}>
                 <StateAuthorisedSvg className={cx(styles['root__authorised-svg'], styles[`root__authorised-svg--${Config.theme}`])} />
-                <p className={styles.root__subtitle}>{balance?.value} {balance?.suffix}</p>
-                <p className={styles.root__explainer}>View history</p>
+                <div className={styles.root__subtitle}>{balance?.value} {balance?.suffix}</div>
+                <div className={styles.root__explainer}>View history</div>
               </div>
 
               { isTransactionsModalOpen && (
@@ -46,19 +46,19 @@ const RewardsHistory = ({ membershipCard, state, addPaymentCardClickHandler = ()
           ) : (
             <div data-testid='no-transaction-history' className={cx(styles['root__transaction-history'], styles['root__transaction-history--disabled'])}>
               <StateAuthorisedGreySvg />
-              <p className={styles.root__subtitle}>{balance?.value} {balance?.suffix}</p>
-              <p className={cx(styles.root__explainer, styles['root__explainer--desktop-only'])}>No transactions to show</p>
-              <p className={cx(styles.root__explainer, styles['root__explainer--mobile-only'])}>Not available</p>
+              <div className={styles.root__subtitle}>{balance?.value} {balance?.suffix}</div>
+              <div className={cx(styles.root__explainer, styles['root__explainer--desktop-only'])}>No transactions to show</div>
+              <div className={cx(styles.root__explainer, styles['root__explainer--mobile-only'])}>Not available</div>
             </div>
           ) }
           { nonActiveVouchers?.length > 0 ? (
             <>
               <div data-testid='non-active-vouchers' className={styles['root__voucher-history']} onClick={() => setNonActiveVouchersModalOpen(true)}>
                 <StateAuthorisedSvg className={cx(styles['root__authorised-svg'], styles[`root__authorised-svg--${Config.theme}`])} />
-                <p className={cx(styles.root__subtitle, styles['root__subtitle--desktop-only'])}>Rewards history</p>
-                <p className={cx(styles.root__subtitle, styles['root__subtitle--mobile-only'])}>History</p>
-                <p className={cx(styles.root__explainer, styles['root__explainer--desktop-only'])}>See your past rewards</p>
-                <p className={cx(styles.root__explainer, styles['root__explainer--mobile-only'])}>Past rewards</p>
+                <div className={cx(styles.root__subtitle, styles['root__subtitle--desktop-only'])}>Rewards history</div>
+                <div className={cx(styles.root__subtitle, styles['root__subtitle--mobile-only'])}>History</div>
+                <div className={cx(styles.root__explainer, styles['root__explainer--desktop-only'])}>See your past rewards</div>
+                <div className={cx(styles.root__explainer, styles['root__explainer--mobile-only'])}>Past rewards</div>
               </div>
               { isNonActiveVouchersModalOpen && (
                 <div data-testid='non-active-vouchers-modal'>
@@ -73,10 +73,10 @@ const RewardsHistory = ({ membershipCard, state, addPaymentCardClickHandler = ()
           ) : (
             <div data-testid='no-non-active-vouchers' className={cx(styles['root__voucher-history'], styles['root__voucher-history--disabled'])}>
               <StateAuthorisedGreySvg />
-              <p className={cx(styles.root__subtitle, styles['root__subtitle--desktop-only'])}>Rewards history</p>
-              <p className={cx(styles.root__subtitle, styles['root__subtitle--mobile-only'])}>History</p>
-              <p className={cx(styles.root__explainer, styles['root__explainer--desktop-only'])}>No vouchers to show</p>
-              <p className={cx(styles.root__explainer, styles['root__explainer--mobile-only'])}>Not available</p>
+              <div className={cx(styles.root__subtitle, styles['root__subtitle--desktop-only'])}>Rewards history</div>
+              <div className={cx(styles.root__subtitle, styles['root__subtitle--mobile-only'])}>History</div>
+              <div className={cx(styles.root__explainer, styles['root__explainer--desktop-only'])}>No vouchers to show</div>
+              <div className={cx(styles.root__explainer, styles['root__explainer--mobile-only'])}>Not available</div>
             </div>
           ) }
         </>
@@ -84,34 +84,34 @@ const RewardsHistory = ({ membershipCard, state, addPaymentCardClickHandler = ()
       { state === 'no-payment-cards' && (
         <div data-testid='no-payment-cards' className={styles['root__no-payment-card-state']} onClick={addPaymentCardClickHandler}>
           <StateFailedSvg />
-          <p className={styles.root__subtitle}>Add a payment card</p>
+          <div className={styles.root__subtitle}>Add a payment card</div>
           <div className={styles.root__explainer}>
-            <p className={styles['root__explainer-paragraph']}>To collect rewards you need to add a payment card to { planName }.</p>
-            <p className={styles['root__explainer-paragraph']}>Click here to get started.</p>
+            <div className={styles['root__explainer-paragraph']}>To collect rewards you need to add a payment card to { planName }.</div>
+            <div className={styles['root__explainer-paragraph']}>Click here to get started.</div>
           </div>
         </div>
       ) }
       { state === 'failed' && (
           <div data-testid='failed-state' className={styles['root__failed-state']}>
             <StateFailedSvg />
-            <p className={styles.root__subtitle}>Something's not right</p>
+            <div className={styles.root__subtitle}>Something's not right</div>
             <div className={styles.root__explainer}>
-              <p className={styles['root__explainer-paragraph']}>There was a problem setting up your account.</p>
-              <p className={styles['root__explainer-paragraph']}>We need some additional information to resolve this.</p>
-              <p className={styles['root__explainer-paragraph']}>Click here to resolve.</p>
+              <div className={styles['root__explainer-paragraph']}>There was a problem setting up your account.</div>
+              <div className={styles['root__explainer-paragraph']}>We need some additional information to resolve this.</div>
+              <div className={styles['root__explainer-paragraph']}>Click here to resolve.</div>
             </div>
           </div>
       ) }
       { state === 'pending' && (
           <div data-testid='pending-state' className={styles['root__pending-state']}>
             <StatePendingSvg />
-            <p className={styles.root__subtitle}>Pending</p>
+            <div className={styles.root__subtitle}>Pending</div>
             <div className={styles.root__explainer}>
-              <p className={styles['root__explainer-paragraph']}>We are getting everything ready for you.</p>
-              <p className={styles['root__explainer-paragraph']}>
+              <div className={styles['root__explainer-paragraph']}>We are getting everything ready for you.</div>
+              <div className={styles['root__explainer-paragraph']}>
                 You will need a payment card to start collecting rewards.
                 This can be done alongside this process.
-              </p>
+              </div>
             </div>
           </div>
       ) }
