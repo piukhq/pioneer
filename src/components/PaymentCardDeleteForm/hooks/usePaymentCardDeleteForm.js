96a8bcdf3b2eae7daa42d47a4f4914350df90267
Lukie Kang <lkang@bink.com>
2021-07-28 14:42:47 +0100

    add plan name suffix and rename similar variables for consistancy

--- src/components/PaymentCardDeleteForm/hooks/usePaymentCardDeleteForm.js
+++ src/components/PaymentCardDeleteForm/hooks/usePaymentCardDeleteForm.js
@@ -2,9 +2,11 @@ import { useCallback, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { actions as paymentCardsActions } from 'ducks/paymentCards'
 import { selectors as membershipCardsSelectors } from 'ducks/membershipCards'
+import { useMembershipCardDetailsByCardId } from 'hooks/useMembershipCardDetailsByCardId'
 import { isPaymentCardExpired, isPaymentCardPending } from 'utils/paymentCards'
 
 export const usePaymentCardDeleteForm = (paymentCardId, onClose, membershipCardId) => {
+  const { planName, planNameSuffix } = useMembershipCardDetailsByCardId()
   const card = useSelector(state => state.paymentCards.cards[paymentCardId])
   const last4Digits = card?.card?.last_four_digits
 
@@ -18,11 +20,8 @@ export const usePaymentCardDeleteForm = (paymentCardId, onClose, membershipCardI
   const error = useSelector(state => state.paymentCards.delete.error)
   const success = useSelector(state => state.paymentCards.delete.success)
 
-  const membershipCardCurrency = useSelector(
+  const currency = useSelector(
     state => membershipCardsSelectors.currency(state, membershipCardId),
-  )
-  const membershipPlanName = useSelector(
-    state => membershipCardsSelectors.plan(state, membershipCardId)?.account?.plan_name,
   )
   const linkedPaymentCards = useSelector(
     state => membershipCardsSelectors.linkedPaymentCards(state, membershipCardId),
@@ -49,8 +48,9 @@ export const usePaymentCardDeleteForm = (paymentCardId, onClose, membershipCardI
     userEnteredLast4Digits,
     setUserEnteredLast4Digits,
     handleDelete,
-    membershipCardCurrency,
-    membershipPlanName,
+    currency,
+    planName,
+    planNameSuffix,
     isLastPaymentCard,
   }
 }
