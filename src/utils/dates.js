648028c5b63bbfa86c19e9647817de18c5bc2f6f
George <godonnell@bink.com>
2021-09-01 14:20:44 +0100

    Removed code to reverse the list of days and months

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -27,9 +27,9 @@ const getDOBDates = () => {
   const years = Array.apply(null, { length: 101 }).map((_, index) => (currentYear - index))
 
   // Allows for a zero to be displayed before single digit numbers
-  const months = Array.apply(null, { length: 12 }).map((_, index) => ('0' + (index + 1)).slice(-2)).reverse()
+  const months = Array.apply(null, { length: 12 }).map((_, index) => ('0' + (index + 1)).slice(-2))
 
-  const days = Array.apply(null, { length: 31 }).map((_, index) => ('0' + (index + 1)).slice(-2)).reverse()
+  const days = Array.apply(null, { length: 31 }).map((_, index) => ('0' + (index + 1)).slice(-2))
 
   // Object keys are used in the expiry date dropdown menus
   return {
