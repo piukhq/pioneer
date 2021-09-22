963c0ac2a0f1b80eb979c90d2c86dfa16eacaa59
Lukie Kang <lkang@bink.com>
2021-09-22 13:33:04 +0100

    Remove redunadant oncloses

--- src/components/Modals/MembershipCardModal/MembershipCardModal.js
+++ src/components/Modals/MembershipCardModal/MembershipCardModal.js
@@ -5,13 +5,13 @@ import { MEMBERSHIP_CARD_IMAGE_TYPES } from 'utils/enums'
 
 import styles from './MembershipCardModal.module.scss'
 
-const MembershipCardModal = ({ onClose, membershipCard }) => {
+const MembershipCardModal = ({ membershipCard }) => {
   const imageUrl = membershipCard?.images?.filter(image => image.type === MEMBERSHIP_CARD_IMAGE_TYPES.ALTERNATIVE)?.[0]?.url
   const backgroundColor = membershipCard?.card?.colour
   const cardId = membershipCard?.card?.membership_id
 
   return (
-    <Modal onClose={onClose} className={styles.root}>
+    <Modal className={styles.root}>
       <div className={styles.root__container}>
         <div className={styles['root__image-container']} style={{ backgroundColor }}>
           <img src={imageUrl} className={styles.root__image} alt='' />
