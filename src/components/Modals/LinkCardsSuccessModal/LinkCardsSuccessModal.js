44548146c22c11ae40caba12abf7c09695d9e282
Lukie Kang <lkang@bink.com>
2021-09-30 09:30:55 +0100

    refactor LinkSuccess to be standalone

--- src/components/Modals/LinkCardsSuccessModal/LinkCardsSuccessModal.js
+++ src/components/Modals/LinkCardsSuccessModal/LinkCardsSuccessModal.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import Modal from 'components/Modal'
 import Button from 'components/Button'
+import { useModals } from 'hooks/useModals'
 
 import styles from './LinkedCardsSuccessModal.module.scss'
 
-const LinkCardsSuccessModal = ({ onClose }) => (
-  <Modal onClose={onClose}>
-    <Modal.Header>Card linked</Modal.Header>
-    <div className={styles.root__paragraph}>
-      Your credit/debit card is now linked! You can now collect points and rewards when you spend.
-    </div>
-    <Button className={styles.root__button} onClick={onClose}>Dismiss</Button>
-  </Modal>
-)
+const LinkCardsSuccessModal = ({ onClose }) => {
+  const { dispatchModal } = useModals()
+  const handleDismissButtonClick = useCallback(() => { dispatchModal('NO_MODAL') }, [dispatchModal])
+  return (
+    <Modal onClose={onClose}>
+      <Modal.Header>Card linked</Modal.Header>
+      <div className={styles.root__paragraph}>
+        Your credit/debit card is now linked! You can now collect points and rewards when you spend.
+      </div>
+      <Button className={styles.root__button} onClick={handleDismissButtonClick}>Dismiss</Button>
+    </Modal>
+  )
+}
 
 export default LinkCardsSuccessModal
