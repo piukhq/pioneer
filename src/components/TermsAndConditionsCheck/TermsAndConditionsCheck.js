8f467db22a71923f5d7a6b55150653f1aa238513
Lukie Kang <lkang@bink.com>
2021-10-14 15:58:25 +0100

    add initial tests for TermsAndConditionsCheck

--- src/components/TermsAndConditionsCheck/TermsAndConditionsCheck.js
+++ src/components/TermsAndConditionsCheck/TermsAndConditionsCheck.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Button from 'components/Button'
-import useBinkTermsAndConditions from './hooks/useBinkTermsAndConditions'
-import useLogout from 'hooks/useLogout'
-import useCheckSessionEnded from 'hooks/useCheckSessionEnded'
+import { useTermsAndConditionsCheck } from './hooks/useTermsAndConditionsCheck'
+import { useLogout } from 'hooks/useLogout'
+import { useCheckSessionEnded } from 'hooks/useCheckSessionEnded'
 import styles from './TermsAndConditionsCheck.module.scss'
 
 // TODO: Check with Jack determine whether to display 'a' or 'an' either as a result from the API
@@ -10,7 +10,7 @@ import styles from './TermsAndConditionsCheck.module.scss'
 const TermsAndConditionsCheck = ({ heading, paragraphOne, paragraphTwoPrefix }) => {
   useCheckSessionEnded() // TODO: Temporary redirect for Web-464
 
-  const { acceptTerms, postError } = useBinkTermsAndConditions()
+  const { acceptTerms, postError } = useTermsAndConditionsCheck()
   const { logout } = useLogout()
 
   const errorMessage = postError ? 'Something went wrong. Please try again.' : null
@@ -19,7 +19,7 @@ const TermsAndConditionsCheck = ({ heading, paragraphOne, paragraphTwoPrefix }) 
       <h1 className={styles.root__heading}>{heading}</h1>
       <div className={styles.root__description}>
         {paragraphOne && <div className={styles.root__paragraph}>{paragraphOne}</div>}
-        <div className={styles.root__paragraph}>{paragraphTwoPrefix} you need to accept the <a className={styles.root__url} href='https://bink.com/terms-and-conditions/' target='_blank' rel='noreferrer'>Bink Terms & Conditions.</a> You only need to do this once.</div>
+        <div className={styles.root__paragraph} data-testid='paragraphTwo'>{paragraphTwoPrefix} you need to accept the <a className={styles.root__url} href='https://bink.com/terms-and-conditions/' target='_blank' rel='noreferrer'>Bink Terms & Conditions.</a> You only need to do this once.</div>
         <div className={styles.root__paragraph}>Please also read the <a className={styles.root__url} href='https://bink.com/privacy-policy/' target='_blank' rel='noreferrer'>Bink Privacy Policy</a> for further details on how your data will be processed.</div>
       </div>
       <div className={styles.root__buttons}>
