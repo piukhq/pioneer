4d5b0c54788c3be6d089d2444f49c621bf84c000
George <godonnell@bink.com>
2021-10-14 14:27:09 +0100

    Refactored the PaymentCardAddForm and paymentCardInputGroup components to be less relient on one another when styling the UI elements. Also adjusted the hook logic to keep the Spreedly functions more centralized

--- src/components/PaymentCardAddForm/hooks/usePaymentCardAddForm.js
+++ src/components/PaymentCardAddForm/hooks/usePaymentCardAddForm.js
@@ -1,6 +1,5 @@
-import { useEffect, useState, useCallback } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { actions as paymentCardsActions } from 'ducks/paymentCards'
+import { useState, useCallback } from 'react'
+import { useSelector } from 'react-redux'
 import { isValidName, isValidExpiry } from 'utils/validation'
 
 export const usePaymentCardAddForm = (onClose) => { // TODO: move onClose functionality inside component as part of refactor. Causes card number styling issue doing so atm.
@@ -9,102 +8,53 @@ export const usePaymentCardAddForm = (onClose) => { // TODO: move onClose functi
   const [fullNameError, setFullNameError] = useState(undefined)
   const [expiryError, setExpiryError] = useState(undefined)
   const [isLoading, setIsLoading] = useState(false)
-  const [isCardNumberValid, setIsCardNumberValid] = useState(false)
-  const [isCardTypeValid, setIsCardTypeValid] = useState(false)
   const [cardNumberError, setCardNumberError] = useState(false)
   const [genericSpreedlyError, setGenericSpreedlyError] = useState(false)
   const [isSpreedlyNumberValid, setIsSpreedlyNumberValid] = useState(false)
 
-  const dispatch = useDispatch()
+  const spreedlyNumberValidation = useCallback((isValid) => { // passed to useSpreedly for response
+    setIsSpreedlyNumberValid(isValid)
+  }, [])
 
-  // TODO: Validation and associated styling of the spreedly card number field is split between functions here and in useSpreedlyCardNumber. Recommend refactoring to avoid duplicate code/ease of understanding
+  const handleErrors = useCallback((errors) => {
+    setIsLoading(false)
 
-  const spreedlyNumberValidation = (validationField) => { // passed to useSpreedlyCardNumber for response
-    setIsSpreedlyNumberValid(validationField)
-  }
+    let anyNonFieldRelatedError = false
+    let anyFieldRelatedError = false
+    //   Most of field related errors here would mean that local validation passed while Spreedly returned an error.
+    //   Once / if we'll have any logging implemented it would be worth logging it for refining further the local validation.
+    errors.forEach(error => {
+      console.error(error)
 
-  const handlePaymentCardBlur = useCallback(() => {
-    if (!isCardNumberValid) {
-      setCardNumberError('Invalid card number')
-    } else if (!isCardTypeValid) {
-      setCardNumberError('You can only add Visa, Mastercard or American Express cards')
-    } else {
-      setCardNumberError(false)
-    }
-  }, [isCardNumberValid, isCardTypeValid, setCardNumberError])
+      switch (error.attribute) {
+        case 'month':
+        case 'year':
+          anyFieldRelatedError = true
+          setExpiryError('Invalid date')
+          break
+        case 'first_name':
+        case 'last_name':
+          anyFieldRelatedError = true
+          setFullNameError('Invalid name')
+          break
+        case 'number':
+          anyFieldRelatedError = true
+          setCardNumberError('Invalid card number')
+          break
+        default:
+          anyNonFieldRelatedError = true
+      }
 
-  const handlePaymentCardChange = useCallback(({ isCardNumberValid, isCardTypeValid }) => {
-    setCardNumberError(false)
-    setIsCardNumberValid(isCardNumberValid)
-    setIsCardTypeValid(isCardTypeValid)
-  }, [setCardNumberError])
-
-  useEffect(() => {
-    const Spreedly = window.Spreedly
-
-    Spreedly.on('paymentMethod', async function (token, pmData) {
-      const newCard = await dispatch(paymentCardsActions.addPaymentCard(
-        token,
-        pmData.last_four_digits,
-        pmData.first_six_digits,
-        pmData.month,
-        pmData.year,
-        pmData.country,
-        pmData.currency_code,
-        pmData.full_name,
-        pmData.card_type,
-        pmData.payment_method_type,
-        pmData.fingerprint,
-      ))
-      setIsLoading(false)
-
-      if (newCard) {
-        onClose && onClose()
+      if (anyNonFieldRelatedError && !anyFieldRelatedError) {
+        setGenericSpreedlyError('Something went wrong. Please try again later')
       }
     })
+  }, [])
 
-    Spreedly.on('errors', function (errors) {
-      setIsLoading(false)
-
-      let anyNonFieldRelatedError = false
-      let anyFieldRelatedError = false
-      //   Most of field related errors here would mean that local validation passed while Spreedly returned an error.
-      //   Once / if we'll have any logging implemented it would be worth logging it for refining further the local validation.
-      errors.forEach(error => {
-        switch (error.attribute) {
-          case 'month':
-          case 'year':
-            anyFieldRelatedError = true
-            setExpiryError('Invalid date')
-            break
-          case 'first_name':
-          case 'last_name':
-            anyFieldRelatedError = true
-            setFullNameError('Invalid name')
-            break
-          case 'number':
-            anyFieldRelatedError = true
-            setCardNumberError('Invalid card number')
-            break
-          default:
-            anyNonFieldRelatedError = true
-        }
-
-        if (anyNonFieldRelatedError && !anyFieldRelatedError) {
-          setGenericSpreedlyError('Something went wrong. Please try again later')
-        }
-      })
-
-      for (let i = 0; i < errors.length; i++) {
-        const error = errors[i]
-        console.log(error)
-      }
-    })
-
-    return () => {
-      Spreedly.removeHandlers()
-    }
-  }, [dispatch, onClose])
+  const handleSuccess = useCallback(() => {
+    setIsLoading(false)
+    onClose && onClose()
+  }, [onClose])
 
   const handleExpiryChange = (key, event) => {
     const newExpiry = {
@@ -119,10 +69,10 @@ export const usePaymentCardAddForm = (onClose) => { // TODO: move onClose functi
     }
   }
 
-  const handleNameChange = (event) => {
+  const handleNameChange = useCallback((event) => {
     setFullName(event.target.value)
     setFullNameError(undefined)
-  }
+  }, [])
 
   const handleNameBlur = useCallback(() => {
     const errorMessage = isValidName(fullName) ? undefined : 'Invalid name'
@@ -130,7 +80,9 @@ export const usePaymentCardAddForm = (onClose) => { // TODO: move onClose functi
   }, [fullName])
 
   const isNameValid = useCallback(() => isValidName(fullName), [fullName])
+
   const isExpiryValid = useCallback(() => isValidExpiry(expiry), [expiry])
+
   const isValidForm = useCallback(() => {
     return isSpreedlyNumberValid && isNameValid() && isExpiryValid()
   }, [isSpreedlyNumberValid, isNameValid, isExpiryValid])
@@ -172,8 +124,8 @@ export const usePaymentCardAddForm = (onClose) => { // TODO: move onClose functi
     handleNameChange,
     handleNameBlur,
     cardNumberError,
-    handlePaymentCardChange,
-    handlePaymentCardBlur,
+    handleErrors,
+    handleSuccess,
     genericSpreedlyError,
     genericBinkError,
     isNameValid,
