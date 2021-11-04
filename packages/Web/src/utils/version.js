35c8486b3d408bba1e68ad36fa552e2afc120612
Lukie Kang <lkang@bink.com>
2021-11-04 11:43:34 +0000

    covert to async function return

--- packages/Web/src/utils/version.js
+++ packages/Web/src/utils/version.js
@@ -1,25 +1,11 @@
 
 export const getServerVersionNumber = async () => {
   const { theme } = Config
-  const versionFilePath = `/mr-316/${theme}/version.json` // test line, TODO: use commented version below
+  const versionFilePath = `/mr-316/${theme}/version.json`
   console.log(versionFilePath)
-  const sha = await fetch(versionFilePath)
+  const tag = await fetch(versionFilePath)
     .then(response => response.json())
     .then(json => json?.sha)
     .catch((e) => console.error(e))
-  console.log(sha + 'is sha')
-  return sha
+  return tag
 }
-
-// export const getServerVersionNumber = async () => {
-//   const { env, theme } = Config
-//   if (env === 'development') {
-//     return null
-//   } else {
-//     const versionFilePath = `/${env}/${theme}/version.json`
-//     return axios(versionFilePath)
-//       .then(response => response.json())
-//       .then(json => json.tag)
-//       .catch((e) => console.error(e))
-//   }
-// }
