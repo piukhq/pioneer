fe3a2de763f163e1144b1e1f6cd6ecbad1ea1690
Lukie Kang <lkang@bink.com>
2021-11-16 07:04:45 +0000

    centralise commonly used elements

--- packages/Web/src/components/MembershipCardContainer/components/UnauthorisedState/UnauthorisedState.js
+++ packages/Web/src/components/MembershipCardContainer/components/UnauthorisedState/UnauthorisedState.js
@@ -26,43 +26,47 @@ const UnauthorisedState = ({ membershipCard, state }) => {
       </div>
   )
   const renderAccountNotRegisteredFailedState = () => (
-    <div data-testid='failed-state' className={styles['root__failed-state']}>
-      <StateFailedSvg key={state} />
+    <>
       <div className={styles.root__subtitle}>{reasonCode === accountNotRegisteredCode ? 'Account not registered' : 'Error'}</div>
       <div className={styles.root__explainer}>
         <div className={styles['root__explainer-paragraph']}>Try again later</div>
       </div>
-    </div>
+    </>
   )
   const renderEnrolRejectedFailedState = () => (
-    <div data-testid='failed-state' className={styles['root__failed-state']}>
-      <StateFailedSvg key={state} />
+    <>
       <div className={styles.root__subtitle}>{reasonCode === enrolRejectedCode ? 'Enrol failed' : 'Account already exists'}</div>
       <div className={styles.root__explainer}>
         <a className={styles['root__explainer-paragraph']} href='https://help.bink.com/hc/en-gb/requests/new?' target='_blank' rel='noreferrer'>Contact support</a>
       </div>
-   </div>
+    </>
   )
-  const renderGeneralFailedStateState = () => (
-    <div data-testid='failed-state' className={styles['root__failed-state']}>
-    <StateFailedSvg key={state} />
-    <div className={styles.root__subtitle}>Something's not right</div>
-    <div className={styles.root__explainer}>
-      <div className={styles['root__explainer-paragraph']}>There was a problem setting up your account.</div>
-      <div className={styles['root__explainer-paragraph']}>We need some additional information to resolve this.</div>
-    </div>
-  </div>
+  const renderGeneralFailedState = () => (
+    <>
+      <div className={styles.root__subtitle}>Something's not right</div>
+      <div className={styles.root__explainer}>
+        <div className={styles['root__explainer-paragraph']}>There was a problem setting up your account.</div>
+        <div className={styles['root__explainer-paragraph']}>We need some additional information to resolve this.</div>
+      </div>
+    </>
   )
 
   if (pendingCodes.includes(reasonCode)) {
     return renderPendingState()
-  } else if (errorCodes.includes(reasonCode) || reasonCode === accountNotRegisteredCode) {
-    return renderAccountNotRegisteredFailedState()
-  } else if (reasonCode === enrolRejectedCode || reasonCode === accountAlreadyExists) {
-    return renderEnrolRejectedFailedState()
-  } else {
-    return renderGeneralFailedStateState()
   }
+
+  const renderFailState = () => {
+    if (errorCodes.includes(reasonCode) || reasonCode === accountNotRegisteredCode) return renderAccountNotRegisteredFailedState()
+    if (reasonCode === enrolRejectedCode || reasonCode === accountAlreadyExists) return renderEnrolRejectedFailedState()
+    return renderGeneralFailedState()
+  }
+
+  return (
+    <div data-testid='failed-state' className={styles['root__failed-state']}>
+      <StateFailedSvg key={state} />
+      {renderFailState()}
+    </div>
+  )
 }
 
 export default UnauthorisedState
