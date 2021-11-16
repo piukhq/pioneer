d5cb826cb07e388143b752cb927bf8ef113844d7
Lukie Kang <lkang@bink.com>
2021-11-16 11:16:32 +0000

    fix linting issue

--- packages/Web/src/components/MembershipCardContainer/components/AuthorisedState/AuthorisedState.js
+++ packages/Web/src/components/MembershipCardContainer/components/AuthorisedState/AuthorisedState.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 import cx from 'classnames'
 import NonActiveVouchersModal from 'components/Modals/NonActiveVouchersModal'
 import TransactionsRewardsEmptyStateModal from 'components/Modals/TransactionsRewardsEmptyStateModal'
