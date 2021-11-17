416001aad092ce85da8c6b8e11f1ef86391baf68
Lukie Kang <lkang@bink.com>
2021-11-17 16:04:17 +0000

    finalise version api

--- packages/Web/src/api/version.js
+++ packages/Web/src/api/version.js
@@ -1,10 +1,11 @@
 
 export const getServerVersion = async () => {
   const { env, theme } = Config
-  let versionFilePath = `/${env}/${theme}/version.json`
-  if (env === 'development') { // Temp placeholder for MR Testing, delete for final version
-    versionFilePath = `/mr-316/${theme}/version.json`
+  if (env === 'development') { // Development does not use tags
+    return 'development'
   }
+  const versionFilePath = `/${env}/${theme}/version.json`
+
   const tag = await fetch(versionFilePath)
     .then(response => response.json())
     .then(json => json.sha) // Temp testing with SHA to return a value in MR testing
