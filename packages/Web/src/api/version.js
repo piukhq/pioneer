b85cf02b6763f4dfa4aba9768d8edda256b5d73b
Lukie Kang <lkang@bink.com>
2021-11-17 16:09:41 +0000

    switch to tag instad of sha

--- packages/Web/src/api/version.js
+++ packages/Web/src/api/version.js
@@ -1,14 +1,14 @@
 
 export const getServerVersion = async () => {
   const { env, theme } = Config
-  if (env === 'development') { // Development does not use tags
+  if (env === 'development') { // Development does not use tags so returns generic 'development' version
     return 'development'
   }
   const versionFilePath = `/${env}/${theme}/version.json`
 
   const tag = await fetch(versionFilePath)
     .then(response => response.json())
-    .then(json => json.sha) // Temp testing with SHA to return a value in MR testing
+    .then(json => json.tag)
     .catch((e) => console.error(e))
   return tag
 }
