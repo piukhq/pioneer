818eaf46afd365e16bc98bb619d1f4f8550aac23
Lukie Kang <lkang@bink.com>
2021-11-24 15:40:41 +0000

    revise uselogout to remve console.log

--- packages/Web/src/hooks/useLogout.js
+++ packages/Web/src/hooks/useLogout.js
@@ -6,7 +6,6 @@ export const useLogout = () => {
   const dispatch = useDispatch()
   const history = useHistory()
   const logout = async () => {
-    console.log(history, ' is his')
     await dispatch(usersActions.logout())
     window.location.href = '/'
   }
