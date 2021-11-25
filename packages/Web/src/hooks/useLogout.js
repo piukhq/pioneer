ad7d86abe5726d890da29669b5b2db489a1c00a7
Lukie Kang <lkang@bink.com>
2021-11-25 09:58:17 +0000

    fix liniting issues

--- packages/Web/src/hooks/useLogout.js
+++ packages/Web/src/hooks/useLogout.js
@@ -1,10 +1,8 @@
 import { useDispatch } from 'react-redux'
-import { useHistory } from 'react-router-dom'
 import { actions as usersActions } from 'ducks/users'
 
 export const useLogout = () => {
   const dispatch = useDispatch()
-  const history = useHistory()
   const logout = async () => {
     await dispatch(usersActions.logout())
     window.location.href = '/'
