6111b2536aa2b9dc28d06609a01b24ad27424677
Lukie Kang <lkang@bink.com>
2021-11-26 09:58:39 +0000

    temp tweak of logout function

--- packages/Web/src/hooks/useLogout.js
+++ packages/Web/src/hooks/useLogout.js
@@ -2,10 +2,11 @@ import { useDispatch } from 'react-redux'
 import { actions as usersActions } from 'ducks/users'
 
 export const useLogout = () => {
+  const { env, theme } = Config
   const dispatch = useDispatch()
   const logout = () => {
     dispatch(usersActions.logout())
-    window.location.href = '/'
+    window.location.href = `/${env}/${theme}`
   }
   return {
     logout,
