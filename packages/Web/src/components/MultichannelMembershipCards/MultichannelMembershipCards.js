c9dc53c9208128cb231cf0d1e620a191100e0b13
George <godonnell@bink.com>
2021-12-07 14:22:56 +0000

    Readjusted the empty wallet screen to match the abstract spec

--- packages/Web/src/components/MultichannelMembershipCards/MultichannelMembershipCards.js
+++ packages/Web/src/components/MultichannelMembershipCards/MultichannelMembershipCards.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import { isMobile, osName } from 'react-device-detect'
 import { useSelector } from 'react-redux'
+import cx from 'classnames'
 import AccountMenu from 'components/AccountMenu'
 import LoadingIndicator from 'components/LoadingIndicator'
 import TermsAndConditionsCheck from 'components/TermsAndConditionsCheck'
+import MembershipCard from './components/MembershipCard'
 import { useMembershipCardsState } from 'hooks/membershipCards'
-import { ReactComponent as FuelGaugeSvg } from 'images/fuel.svg'
+import { ReactComponent as EmptyWalletSvg } from 'images/empty-wallet.svg'
+import { MOBILE_OS_NAMES as osEnums } from 'utils/enums'
 
 import styles from './MultichannelMembershipCards.module.scss'
-import MembershipCard from './components/MembershipCard'
 
 const MultichannelMembershipCards = () => {
   const { membershipCards, loading } = useMembershipCardsState()
@@ -41,18 +44,69 @@ const MultichannelMembershipCards = () => {
     return <TermsAndConditionsCheck {...getTermsAndConditionsProps()} />
   }
 
-  const renderMembershipCardsContent = () => {
-    if (membershipCards.length > 0) {
-      return (
-        membershipCards.map((card, index) => (
-            <MembershipCard card={card} key={index} />
-        ))
-      )
+  const renderAppLinks = () => {
+    const iosLink = (
+      <a href='https://apps.apple.com/gb/app/bink-loyalty-rewards-wallet/id1142153931' target="_blank" rel="noreferrer">
+        <div className={cx(styles['root__app-link'], styles['root__app-link--ios-store'])} />
+      </a>
+    )
+
+    const androidLink = (
+      <a href='https://play.google.com/store/apps/details?id=com.bink.wallet&hl=en_GB&gl=US' target="_blank" rel="noreferrer">
+        <div className={cx(styles['root__app-link'], styles['root__app-link--android-store'])} />
+      </a>
+    )
+
+    if (isMobile) {
+      return osName === osEnums.IOS ? iosLink : androidLink
     }
+
+    return (
+      <>
+        {iosLink}
+        {androidLink}
+      </>
+    )
+  }
+
+  const renderMembershipCardsContent = () => {
+    return (
+      <div className={styles['root__cards-container']} data-testid='cards-container'>
+        { membershipCards.map((card, index) => <MembershipCard card={card} key={index} />) }
+      </div>
+    )
+  }
+
+  const renderEmptyState = () => {
     return (
       <div className={styles['root__empty-state-container']} data-testid='empty-state-container' id='multichannel-membership-cards-empty-state-container'>
-        <FuelGaugeSvg className={styles['root__empty-state-icon']} data-testid='empty-state-icon' />
-        <div className={styles['root__empty-state-text']}>You have no cards</div>
+        <EmptyWalletSvg className={styles['root__empty-state-icon']} data-testid='empty-state-icon' />
+
+        <div className={styles['root__empty-state-text-container']}>
+          <div className={styles['root__empty-state-title']}>Your wallet is empty</div>
+          <div className={styles['root__empty-state-description']}>Add loyalty cards in the Bink mobile app to start earning rewards</div>
+        </div>
+
+        <div className={styles['root__app-info-container']}>
+          <div className={styles['root__app-info-header']}>
+            <div className={styles['root__app-icon']} />
+
+            <div className={styles['root__app-info-text-container']}>
+              <div className={styles['root__app-info-text-header']}>Download Bink Loyalty & Rewards Wallet</div>
+              <div>Store, View, Link your Loyalty</div>
+            </div>
+          </div>
+
+          <div className={styles['root__app-images-container']}>
+            <div className={cx(styles['root__app-image'], styles['root__app-image--image-1'])} />
+            <div className={cx(styles['root__app-image'], styles['root__app-image--image-2'])} />
+            <div className={cx(styles['root__app-image'], styles['root__app-image--image-3'])} />
+          </div>
+
+          <div className={styles['root__app-links-container']}>
+            { renderAppLinks() }
+          </div>
+        </div>
       </div>
     )
   }
@@ -68,9 +122,8 @@ const MultichannelMembershipCards = () => {
       </div>
 
       <h1 className={styles.root__heading}>Wallet</h1>
-      <div className={styles['root__cards-container']} data-testid='cards-container'>
-        {renderMembershipCardsContent()}
-      </div>
+
+      { membershipCards.length > 0 ? renderMembershipCardsContent() : renderEmptyState() }
     </div>
   )
 }
