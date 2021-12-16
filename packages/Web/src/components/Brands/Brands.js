2f01e9230ee777c4f9f4f0f22868a0b9daf80f1b
Lukie Kang <lkang@bink.com>
2021-12-16 15:07:09 +0000

    fix indentation and wierd css usage

--- packages/Web/src/components/Brands/Brands.js
+++ packages/Web/src/components/Brands/Brands.js
@@ -10,15 +10,15 @@ const Brands = ({ plans }) => {
   return (
     <div className={styles.root}>
        <div className={styles.root__header}>
-          <AccountMenu />
           <LeftChevronSvg className={styles['root__left-arrow']} onClick={() => history.push('/membership-cards')} />
+          <AccountMenu />
       </div>
 
       <h1 className={styles.root__heading}>Add Loyalty Card</h1>
       <div className={styles.root__description}>Add your current loyalty cards to store your barcode in Bink</div>
       <div className={styles['root__brands-container']}>
         {plans.map((plan, index) => (
-            <Brand plan={plan} key={index}/>
+          <Brand plan={plan} key={index}/>
         ))}
       </div>
     </div>
