493af7011d2a163cbedab711cf4f8e451ff87b01
George <godonnell@bink.com>
2021-12-20 11:59:29 +0000

    Temporarily remove version check

--- packages/Web/src/hooks/useActivityMonitoring.js
+++ packages/Web/src/hooks/useActivityMonitoring.js
@@ -1,12 +1,13 @@
 import { useCallback, useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+// import { useDispatch, useSelector } from 'react-redux'
 import { usePrevious } from 'react-use'
 import { useUserState } from 'hooks/users'
 import { useLogout } from 'hooks/useLogout'
 import { getAuthToken } from 'utils/storage'
-import { getServerVersion } from 'api/version'
+// import { getServerVersion } from 'api/version'
 
-import { selectors as versionSelectors, actions as versionActions } from 'ducks/clientVersion'
+// import { selectors as versionSelectors, actions as versionActions } from 'ducks/clientVersion'
 import {
   actions as allActions,
 } from 'ducks/all'
@@ -14,10 +15,10 @@ import {
 import { convertMinutesToMilliseconds } from 'utils/format'
 
 export const useInitialVersionCheck = () => {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(versionActions.setClientVersion())
-  }, [dispatch])
+  // const dispatch = useDispatch()
+  // useEffect(() => {
+  //   dispatch(versionActions.setClientVersion())
+  // }, [dispatch])
 }
 export function useActivityCheck () {
   const [isIdle, setIsIdle] = useState(false)
@@ -28,19 +29,20 @@ export function useActivityCheck () {
   const setIdle = () => setIsIdle(true)
   const setActive = () => setIsIdle(false)
 
-  const clientVersion = useSelector(state => versionSelectors.clientVersion(state))
+  // const clientVersion = useSelector(state => versionSelectors.clientVersion(state))
   const previousIsIdle = usePrevious(isIdle)
   const onActiveCheck = useCallback(async () => {
-    const currentServerVersion = await getServerVersion()
+    // const currentServerVersion = await getServerVersion()
 
     if (!apiKey || apiKey !== getAuthToken()) {
       logout()
-    } else if (clientVersion !== currentServerVersion) {
-      window.location.reload(true)
+    // } else if (clientVersion !== currentServerVersion) {
+    //   window.location.reload(true)
     } else {
       dispatch(allActions.fullRefresh())
     }
-  }, [apiKey, clientVersion, dispatch, logout])
+  // }, [apiKey, clientVersion, dispatch, logout])
+  }, [apiKey, dispatch, logout])
 
   useEffect(() => {
     if (!isIdle && previousIsIdle) {
