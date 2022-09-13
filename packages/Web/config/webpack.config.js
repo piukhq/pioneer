992f460ba7fb6d6fa34b3151fcc54110ebbc8997
Lukie Kang <lkang@bink.com>
2022-09-13 16:10:27 +0100

    fix image loading issue

--- packages/Web/config/webpack.config.js
+++ packages/Web/config/webpack.config.js
@@ -395,12 +395,8 @@ module.exports = function (webpackEnv) {
             // smaller than specified limit in bytes as data URLs to avoid requests.
             // A missing `test` is equivalent to a match.
             {
-              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-              loader: require.resolve('url-loader'),
-              options: {
-                limit: imageInlineSizeLimit,
-                name: 'static/media/[name].[hash:8].[ext]',
-              },
+              test: /\.(png|jpg|jpeg|gif)$/i,
+              type: 'asset/resource',
             },
             // Process application JS with Babel.
             // The preset includes JSX, Flow, TypeScript, and some ESnext features.
