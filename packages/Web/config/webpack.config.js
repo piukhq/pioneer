63bf9719abd3929e039587ec5a734d14e4e80157
Lukie Kang <lkang@bink.com>
2022-09-14 10:29:41 +0100

    replace url-loader with asset loader for avifs

--- packages/Web/config/webpack.config.js
+++ packages/Web/config/webpack.config.js
@@ -384,12 +384,7 @@ module.exports = function (webpackEnv) {
             // https://github.com/jshttp/mime-db
             {
               test: [/\.avif$/],
-              loader: require.resolve('url-loader'),
-              options: {
-                limit: imageInlineSizeLimit,
-                mimetype: 'image/avif',
-                name: 'static/media/[name].[hash:8].[ext]',
-              },
+              type: 'asset/resource',
             },
             // "url" loader works like "file" loader except that it embeds assets
             // smaller than specified limit in bytes as data URLs to avoid requests.
